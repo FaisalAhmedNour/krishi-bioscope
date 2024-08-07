a341324aefbe39bced912f7e0eec36e55abaed53
Faisal Ahmed <faisalahmed61919112@gmail.com>
2024-08-07 03:01:38 -0700

    url changed

--- lib/getAllVideos.ts
+++ lib/getAllVideos.ts
@@ -1,6 +1,6 @@
 export default async function getAllVideos(category?: string) {
     // console.log('category : ', category);
     // const response = await fetch('/api/videos' + (category ? `?category=${category}` : ''));
-    const response = await fetch('http://localhost:3000/api/videos');
+    const response = await fetch('https://krishi-bioscope-2-r8p2gn2t8-faisalahmednours-projects.vercel.app/api/videos');
     return response.json();
 }
