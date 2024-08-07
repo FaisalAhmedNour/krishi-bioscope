4316a4b0d01e9d85b10bb2376c12e25938f046b1
Faisal Ahmed <faisalahmed61919112@gmail.com>
2024-08-07 04:40:25 -0700

    api try catch added

--- lib/getAllCategories.ts
+++ lib/getAllCategories.ts
@@ -1,4 +1,13 @@
 export default async function getAllCategories() {
-    const response = await fetch('http://localhost:3000/api/categories');
-    return response.json();
+    try {
+        const response = await fetch('http://localhost:3000/api/categories');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+        return null;
+      }
 }
--- lib/getAllVideos.ts
+++ lib/getAllVideos.ts
@@ -1,6 +1,13 @@
-export default async function getAllVideos(category?: string) {
-    // console.log('category : ', category);
-    // const response = await fetch('/api/videos' + (category ? `?category=${category}` : ''));
-    const response = await fetch('http://localhost:3000/api/videos');
-    return response.json();
+export default async function getAllVideos() {
+    try {
+        const response = await fetch('http://localhost:3000/api/videos');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+        return null;
+      }
 }
